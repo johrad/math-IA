2103c5446159324c168acea3d2b6ecb001bb2196
johrad <jpdragic@gmail.com>
2018-12-02 20:58:08 +0100

    first draft done

--- decay.py
+++ decay.py
@@ -1,8 +1,9 @@
-# Python code for gathering Math IA data, decay 
+# Python code for gathering Math IA data, decay
 
-amount = 100.0
-table_data = []
-table_data.append(amount)
+amount = 100.0  # Setting initial amount
+table_data = []  # Declaring a new table
+table_data.append(amount)  # adding initial amount to table
+
 
 def decay(amount):
     newAmount = amount/2.0
@@ -10,14 +11,16 @@ def decay(amount):
     print(newAmount)
 
 
-while amount > 0.001:
+while amount > 0.01:
     amount = decay(amount)
-    round(amount,2)
     print(amount)
-    
+
     table_data.append(amount)
 
 print("\nThe data consists of: {} entires.".format(len(table_data)))
-i=0 
-for i in range(0,len(table_data)):
-    print("{} & {} \\\\".format(i,str(table_data[i])))
+i = 0
+
+# preparing the data into a LaTeX ready table for the document
+# https://www.latex-project.org/about/
+for i in range(0, len(table_data)):
+    print("{} & {} \\\\".format(i, str(table_data[i])))
